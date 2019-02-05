4ed57d6eb75d64b2ea4dca1f2b96d4a469815251
unknown <2_web9_24@JOVELLANOS.EDU>
2019-02-05 16:22:31 +0100

    primera version

---
+++ articleCsharp.php
@@ -0,0 +1,33 @@
+<?php
+  include('includes/navBar.inc');
+?>
+  <header class="bg-primary text-white">
+    <div class="container text-center">
+      <h1>Bienvenido</h1>
+      <p class="lead">En esta página vamos a desarrollar la historia de varios lenguajes de programación.</p>
+    </div>
+  </header>
+
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>¿Qué es un lenguaje de programación?</h2>
+          <p class="lead justificado">
+            Un lenguaje de programación es un lenguaje formal que especifica una serie de instrucciones 
+            para que una computadora produzca diversas clases de datos. Los lenguajes de programación 
+            pueden usarse para crear programas que pongan en práctica algoritmos específicos los cuales 
+            controlan el comportamiento físico y lógico de una computadora. 
+            Está formado por un conjunto de símbolos y reglas sintácticas y semánticas que definen su estructura 
+            y el significado de sus elementos y expresiones.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+<?php
+  include('includes/footerScripts.inc');
+?>
+</body>
+
+</html>
---
+++ articleJavascript.php
@@ -0,0 +1,73 @@
+<?php
+  include('includes/navBar.inc');
+?>
+  <header class="bg-primary text-white">
+    <div class="container text-center">
+      <h1>JAVASCRIPT</h1>
+      <p class="lead">
+        JavaScript fue desarrollado originalmente por Brendan Eich de Netscape con el nombre de Mocha, 
+        el cual fue renombrado posteriormente a LiveScript, para finalmente quedar como JavaScript.
+      </p>
+    </div>
+  </header>
+
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>¿Qué es javascript?</h2>
+          <p class="lead justificado">
+            JavaScript (abreviado comúnmente JS) es un lenguaje de programación interpretado, 
+            dialecto del estándar ECMAScript. Se define como orientado a objetos,​ basado en prototipos, 
+            imperativo, débilmente tipado y dinámico. Se utiliza principalmente en su forma del 
+            lado del cliente (client-side), implementado como parte de un navegador web permitiendo 
+            mejoras en la interfaz de usuario y páginas web dinámicas aunque existe una forma de 
+            JavaScript del lado del servidor (Server-side JavaScript o SSJS).
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Historia</h2>
+          <p class="lead justificado">
+            A principios de los años 90, la mayoría de usuarios que se conectaban a Internet lo hacían con 
+            módems a una velocidad máxima de 28.8 kbps. En esa época, empezaban a desarrollarse las primeras 
+            aplicaciones web y por tanto, las páginas web comenzaban a incluir formularios complejos.
+          </p>
+          <p class="lead justificado">
+            Con unas aplicaciones web cada vez más complejas y una velocidad de navegación tan lenta, surgió 
+            la necesidad de un lenguaje de programación que se ejecutara en el navegador del usuario. 
+            De esta forma, si el usuario no rellenaba correctamente un formulario, no se le hacía esperar 
+            mucho tiempo hasta que el servidor volviera a mostrar el formulario indicando los errores 
+            existentes. Brendan Eich, un programador que trabajaba en Netscape, pensó que podría 
+            solucionar este problema adaptando otras tecnologías existentes (como ScriptEase) al navegador 
+            Netscape Navigator 2.0, que iba a lanzarse en 1995.
+          </p>
+          <p class="lead justificado">
+            Inicialmente, Eich denominó a su lenguaje 
+            LiveScript. Posteriormente, Netscape firmó una alianza con Sun Microsystems para el desarrollo 
+            del nuevo lenguaje de programación. Además, justo antes del lanzamiento Netscape decidió cambiar 
+            el nombre por el de JavaScript. La razón del cambio de nombre fue exclusivamente por marketing, 
+            ya que Java era la palabra de moda en el mundo informático y de Internet de la época.
+            La primera versión de JavaScript fue un completo éxito y Netscape Navigator 3.0 ya incorporaba 
+            la siguiente versión del lenguaje, la versión 1.1. Al mismo tiempo, Microsoft lanzó JScript con 
+            su navegador Internet Explorer 3. JScript era una copia de JavaScript al que le cambiaron el 
+            nombre para evitar problemas legales. Para evitar una guerra de tecnologías, Netscape decidió 
+            que lo mejor sería estandarizar el lenguaje JavaScript. De esta forma, en 1997 se envió la 
+            especificación JavaScript 1.1 al organismo ECMA European Computer Manufacturers Association).
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+<?php
+  include('includes/footerScripts.inc');
+?>
+</body>
+
+</html>
